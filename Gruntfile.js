116b1e0bf2d53b3d6a1af5db6b5bd80a20d3b716
Maxime Fabre <ehtnam6@gmail.com>
2014-01-08 18:32:32 +0100

    Create base assets files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,15 @@ module.exports = function(grunt) {
 
 	// Load modules
 	grunt.loadNpmTasks('grunt-bower-task');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Project configuration.
 	grunt.initConfig({
+
+		name: 'warkham',
 
 		//////////////////////////////////////////////////////////////////
 		/////////////////////////////// PATHS ////////////////////////////
@@ -41,6 +47,10 @@ module.exports = function(grunt) {
 				files: 'Gruntfile.js',
 				tasks: 'default',
 			},
+			scripts: {
+				files: ['<%= paths.original.js %>/**/*'],
+				tasks: ['js', 'uglify'],
+			},
 		},
 
 		// Assets
@@ -54,6 +64,44 @@ module.exports = function(grunt) {
 			}
 		},
 
+		concat: {
+			css: {
+				dest: '<%= paths.compiled.css %>/<%= name %>.css',
+				src: [
+					'<%= components %>/chosen/chosen.min.css',
+				],
+			},
+			js: {
+				dest: '<%= paths.compiled.js %>/<%= name %>.js',
+				src: [
+					'<%= components %>/jquery/jquery.min.js',
+					'<%= components %>/chosen/chosen.jquery.min.js',
+				],
+			}
+		},
+
+		cssmin: {
+			minify: {
+				expand : true,
+				cwd    : '<%= builds %>',
+				src    : '*.css',
+				dest   : '<%= builds %>',
+				ext    : '.min.css'
+			}
+		},
+
+		uglify: {
+			dist: {
+				files: [{
+					expand : true,
+					cwd    : '<%= paths.compiled.js %>',
+					src    : '*.js',
+					dest   : '<%= paths.compiled.js %>',
+					ext    : '.min.js',
+				}],
+			}
+		},
+
 	});
 
 	////////////////////////////////////////////////////////////////////
@@ -62,5 +110,20 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('default', 'Build assets for local', [
 		'bower',
+		'css',
+		'js',
+	]);
+
+	// Asset types
+	////////////////////////////////////////////////////////////////////
+
+	grunt.registerTask('css', 'Build stylesheets', [
+		'concat:css',
+		'cssmin',
+	]);
+
+	grunt.registerTask('js', 'Build scripts', [
+		'concat:js',
+		'uglify',
 	]);
 };
