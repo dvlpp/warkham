f67df619272fd38be49da443af81ac4c091fdd3f
Maxime Fabre <ehtnam6@gmail.com>
2014-01-08 18:20:03 +0100

    Add Textarea#ui

--- src/Fields/Textarea.php
+++ src/Fields/Textarea.php
@@ -2,11 +2,33 @@
 namespace Warkham\Fields;
 
 use Former\Form\Fields\Textarea as FormerTextarea;
+use Warkham\Traits\WarkhamField;
 
 /**
  * A textarea field
  */
 class Textarea extends FormerTextarea
 {
-	// ...
+	use WarkhamField;
+
+	/**
+	 * The default attributes
+	 *
+	 * @var array
+	 */
+	protected $attributes = array(
+		'class' => 'wkm-textarea',
+	);
+
+	/**
+	 * Set the interface to wrap the textarea in
+	 *
+	 * @param string $interface
+	 *
+	 * @return self
+	 */
+	public function ui($interface)
+	{
+		return $this->setDataAttribute('ui', $interface);
+	}
 }
--- tests/Fields/TextareaTest.php
+++ tests/Fields/TextareaTest.php
@@ -8,13 +8,40 @@ class TextareaTest extends WarkhamTestCase
 	public function testCanCreateTextarea()
 	{
 		$field = $this->warkham->textarea('dummy');
+		$this->assertTextarea($field);
+	}
 
-		$this->assertHtml(array(
+	public function testCanSetInterface()
+	{
+		$field = $this->warkham->textarea('dummy')->ui('markdown');
+		$this->assertTextarea($field, array(
+			'data-ui' => 'markdown',
+		));
+	}
+
+	////////////////////////////////////////////////////////////////////
+	/////////////////////////////// HELPERS ////////////////////////////
+	////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Assert a string matches a textarea field
+	 *
+	 * @param string $field
+	 * @param array  $attributes
+	 *
+	 * @return Assertion
+	 */
+	protected function assertTextarea($field, $attributes = array())
+	{
+		$attributes = array(
 			'tag'        => 'textarea',
 			'id'         => 'dummy',
-			'attributes' => array(
-				'name' => 'dummy',
-			),
-		), $field);
+			'attributes' => array_merge(array(
+				'class' => 'wkm-textarea',
+				'name'  => 'dummy',
+			), $attributes),
+		);
+
+		return $this->assertHtml($attributes, $field);
 	}
 }
