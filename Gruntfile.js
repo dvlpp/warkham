3ad31caf892cb18eff7499e4d78f86b2d1c16253
Maxime Fabre <ehtnam6@gmail.com>
2014-01-14 00:44:27 +0100

    Load Grunt tasks programatically

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,7 @@
 module.exports = function(grunt) {
 
 	// Load modules
-	grunt.loadNpmTasks('grunt-bower-task');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	require('load-grunt-tasks')(grunt);
 
 	// Project configuration.
 	grunt.initConfig({
