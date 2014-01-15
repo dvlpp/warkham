d7a0960e811f8ec43eb19fab15e343a4eb020a72
Maxime Fabre <ehtnam6@gmail.com>
2014-01-16 00:15:51 +0100

    Add bootstrap-wysiwyg and bootstrap-markdown components

--- .grunt/assets/concat.js
+++ .grunt/assets/concat.js
@@ -3,6 +3,7 @@ module.exports = {
 		dest: '<%= paths.compiled.css %>/<%= name %>.css',
 		src: [
 			'<%= components %>/bootstrap/dist/css/bootstrap.css',
+			'<%= components %>/bootstrap-markdown/css/bootstrap-markdown.min.css',
 			'<%= components %>/select2/select2.css',
 			'<%= paths.original.css %>/*.css'
 		],
@@ -11,6 +12,8 @@ module.exports = {
 		dest: '<%= paths.compiled.js %>/<%= name %>.js',
 		src: [
 			'<%= components %>/jquery/jquery.js',
+			'<%= components %>/bootstrap-markdown/js/bootstrap-markdown.js',
+			'<%= components %>/bootstrap-wysiwyg/bootstrap-wysiwyg.js',
 			'<%= components %>/select2/select2.js',
 			'<%= components %>/typeahead.js/dist/typeahead.js',
 			'<%= paths.original.js %>/**/*.js'
