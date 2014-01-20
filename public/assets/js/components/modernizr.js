ae0c1ea888b3ae2d15ed4c5b9450dc79d72d3c36
Maxime Fabre <ehtnam6@gmail.com>
2014-01-21 00:23:09 +0100

    Add jQuery timepicker as a fallback to input[type=time]

--- public/assets/js/components/modernizr.js
+++ public/assets/js/components/modernizr.js
@@ -1,10 +1,29 @@
+var components = 'components';
+
 yepnope([
 	{
-		test     : Modernizr.inputtypes,
-		nope     : ['components/jquery-ui/ui/minified/jquery-ui.min.js', 'components/jquery-ui/themes/ui-lightness/jquery-ui.min.css'],
+		test : Modernizr.inputtypes,
+		nope : [
+			components + '/jquery-ui/themes/ui-lightness/jquery-ui.min.css',
+			components + '/jquery-ui/ui/minified/jquery-ui.min.js',
+			components + '/jquery-timepicker-jt/jquery.timepicker.css',
+			components + '/jquery-timepicker-jt/jquery.timepicker.min.js',
+		],
 		complete : function() {
 			if (!Modernizr.inputtypes) {
+				// Set datepickers
 				$('input[type="date"]').attr('type', 'text').datepicker();
+
+				// Set timepickers
+				$('input[type="time"]').each(function() {
+					var $field = $(this);
+
+					$field.attr('type', 'text').timepicker({
+						maxTime : $field.attr('max'),
+						minTime : $field.attr('min'),
+						step    : $field.attr('step') / 60 || 1,
+					});
+				});
 			}
 		},
 	}
