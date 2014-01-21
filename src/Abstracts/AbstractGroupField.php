0d5c723c2d6765c073759bdefe468933d1bf5c29
Maxime Fabre <ehtnam6@gmail.com>
2014-01-21 01:43:01 +0100

    Add label to AbstractGroupField

--- src/Abstracts/AbstractGroupField.php
+++ src/Abstracts/AbstractGroupField.php
@@ -1,12 +1,18 @@
 <?php
 namespace Warkham\Abstracts;
 
+use Former\Helpers;
 use HtmlObject\Element;
 use HtmlObject\Traits\Tag;
 use Illuminate\Container\Container;
 
 abstract class AbstractGroupField extends Tag
 {
+	/**
+	 * The Container
+	 *
+	 * @var Container
+	 */
 	protected $app;
 
 	/**
@@ -16,23 +22,63 @@ abstract class AbstractGroupField extends Tag
 	 */
 	protected $element = 'div';
 
-  /**
-   * Whether the element is self closing
-   *
-   * @var boolean
-   */
-  protected $isSelfClosing = false;
+	/**
+	 * The current label
+	 *
+	 * @var string
+	 */
+	protected $label;
 
-  /**
-   * Build a new Group field
-   *
-   * @param Container $app
-   * @param string    $label
-   * @param array     $validations
-   */
-  public function __construct(Container $app, $label, $validations)
-  {
-  	$this->app = $app;
-    $this->addClass($this->app['former.framework']->getGroupClasses());
-  }
+	/**
+	 * Whether the element is self closing
+	 *
+	 * @var boolean
+	 */
+	protected $isSelfClosing = false;
+
+	/**
+	 * Build a new Group field
+	 *
+	 * @param Container $app
+	 * @param string    $label
+	 * @param array     $validations
+	 */
+	public function __construct(Container $app, $label, $validations)
+	{
+		$this->app = $app;
+		$this->addClass($this->app['former.framework']->getGroupClasses());
+		$this->label = $label;
+	}
+
+	/**
+	 * Set the Group's label
+	 *
+	 * @param string $label
+	 */
+	public function setLabel($label)
+	{
+		// Create label
+		if (!$label instanceof Element) {
+			$label = Helpers::translate($label);
+		}
+
+		// Set framework classes
+
+		// Bind as child
+		$this->setChild($label, 'label');
+	}
+
+	public function render()
+	{
+		// Create label
+		$label = Helpers::translate($this->label);
+		$label = Element::create('label', $label)->for($label);
+		$label->addClass($this->app['former.framework']->getLabelClasses());
+
+		// Create wrapping div
+		$children = $this->renderChildren();
+		$children = $this->app['former.framework']->wrapField($children);
+
+		return $this->open().$label.$children.$this->close();
+	}
 }
